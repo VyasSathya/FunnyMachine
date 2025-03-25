7bf48aacac526e55c5b93a66fda3e8bb5cc46d9f
Vyas Sathya <vyas.sathya@gmail.com>
2025-03-25 06:08:22 -0700

    updated ui

---
+++ src/mockData/library.js
@@ -0,0 +1,8 @@
+export default [
+    { id: "idea1", type: "idea", label: "🚀 Premise: Dating in the Future", text: "In the future, dating apps..." },
+    { id: "joke1", type: "joke", label: "😂 Joke: Gym Loyalty", text: "I just wanna be hot enough that being loyal matters..." },
+    { id: "bit1", type: "bit", label: "🧩 Bit: Club Culture", text: "I go to a club once a year..." },
+    { id: "set1", type: "set", label: "🎭 Set: Breakup Night", text: "Opener: breakup, Joke: gym loyalty, Closer: club culture" },
+    { id: "special1", type: "special", label: "🎥 Special: Year of Yes", text: "Theme: Trying everything once" }
+  ];
+  
---
+++ src/mockData/newMaterial.js
@@ -0,0 +1,5 @@
+export default [
+    { type: "joke", text: "I miss my ex. But only because she took the Netflix login." },
+    { type: "segue", text: "Leads into ghosting bit..." }
+  ];
+  
